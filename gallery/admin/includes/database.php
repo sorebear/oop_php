ad33c0020bdddc0d8b68cc4034ced48905504335
sorebear <sorenbaird@gmail.com>
2017-12-17 11:21:42 -0800

    Made mysql queries OO

--- gallery/admin/includes/database.php
+++ gallery/admin/includes/database.php
@@ -11,11 +11,13 @@ class Database {
     }
 
     public function open_db_connection() {
-        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        
-        if (mysqli_connect_errno()) {
+        // $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-            die("Database connected failed" . mysqli_error());
+        $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        
+        if ($this->connection->connect_errno) {
+
+            die("Database connected failed" . $this->connection->connect_error);
 
         }
 
@@ -26,14 +28,30 @@ class Database {
 
     public function query($sql) {
 
-        $result = mysqli_query($this->connection, $sql);
-        if (!$result) {
-            die("Query Failed");
-        }
+        $result = $this->connection->query($sql);
+
+        $this->confirm_query($result);
 
         return $result;
 
     }
+
+    private function confirm_query($result) {
+
+        if(!$result) {
+            die("Query Failed" . $this->connection->error);
+        }
+    }
+
+    public function escape_string($string) {
+        $escaped_string = $this->connection->real_escape_string($string);
+        return $escaped_string;
+    }
+
+    public function the_insert_id() {
+        return $this->connection->insert_id;
+    }
+
 }
 
 $database = new Database();
