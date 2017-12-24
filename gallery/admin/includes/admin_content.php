5c9388f863d60050e1141adfd9762f28610a3537
sorebear <sorenbaird@gmail.com>
2017-12-24 15:40:36 -0800

    Uncommented calls in admin_content

--- gallery/admin/includes/admin_content.php
+++ gallery/admin/includes/admin_content.php
@@ -10,9 +10,9 @@
 
             <?php 
             
-                // $user_1 = User::find_user_by_id(1);
+                $user_1 = User::find_user_by_id(1);
                 
-                // print_r($user_1);
+                print_r($user_1);
 
 
             ?>
